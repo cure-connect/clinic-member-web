1f33e3a44e32c719600eef2af789af427a4c727f
wasutha-cureconnect <wasutha.jun@gmail.com>
2025-10-28 14:50:15 +0700

    fix env

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
     react(),
     tailwindcss(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: [
+    ],
+    server: {
+      host: `${env.VITE_HOST}`,
+      port: env.VITE_PORT ? Number(env.VITE_PORT) : 5173,
+      allowedHosts: [
       'looksmart-properties-introducing-habits.trycloudflare.com'
     ],
-  },
+    },
+  }
 })
