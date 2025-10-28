d987544ab9e7a4c35e1c10864a223898ef1bf7e8
wasutha-cureconnect <wasutha.jun@gmail.com>
2025-10-28 15:16:02 +0700

    fix env

--- src/components/Auth/LoginPage.tsx
+++ src/components/Auth/LoginPage.tsx
@@ -22,7 +22,7 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
 
     setTimeout(async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
+        const response = await fetch(`${import.meta.env.VITE_AUTH_LOGIN}/login`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
