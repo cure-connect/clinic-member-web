73c2e6a78387215afdcc971fcbdde7a953990916
wasutha-cureconnect <wasutha.jun@gmail.com>
2025-10-28 15:22:48 +0700

    fix env

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_FRONT_END_TEST: string;
   readonly VITE_PORT: string;
+  readonly VITE_AUTH_LOGIN: string;
 }
 
 interface ImportMeta {
